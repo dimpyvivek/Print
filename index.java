b5a8c874943a6c71b3e00d1a497ec010fa5b6267
Vivek pandey <vp1446392@gmail.com>
2022-11-07 17:29:13 +0530

    first commit

---
+++ index.java
@@ -0,0 +1,23 @@
+
+/**
+ * index
+ */
+public class index {
+
+    
+public static void main(String[] args) {
+      System.out.println("This is a git Program .    ");
+
+
+
+
+}
+
+}
+// echo "# Print" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/dimpyvivek/Print.git
+// git push -u origin main
